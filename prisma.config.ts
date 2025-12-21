895a421f58774280346a21dd72eef28c6792e322
farzah14 <farzahwalker46.com>
2025-12-21 18:59:55 +0700

    Update OAtuh

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,9 @@
-// prisma.config.ts
-import "dotenv/config";
 import { defineConfig } from "@prisma/config";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is missing. Put it in .env (root).');
-}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: { path: "prisma/migrations" },
   datasource: {
-    url: process.env.DATABASE_URL,
+    // migrate/generate CLI akan pakai DIRECT_URL (direct)
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL,
   },
 });
